d0df84b189a34a47bec6e3edad8a8d15f9ce314d
dachir <dodziamouzou@gmail.com>
2025-01-17 18:50:10 +0000

    170120251947

--- erp_space/__init__.py
+++ erp_space/__init__.py
@@ -1,6 +1,10 @@
 __version__ = "0.0.1"
 
+import frappe
 from .erpspace import erpspace
 
 # Register the namespace globally as "erpspace"
 globals()['erpspace'] = erpspace
+
+# Register `erpspace` under `frappe` namespace
+frappe.erpspace = erpspace
--- erp_space/erpspace.py
+++ erp_space/erpspace.py
@@ -1,3 +1,5 @@
+import frappe
+
 class ErpSpace:
     def __init__(self):
         self.functions = {}
@@ -12,5 +14,52 @@ class ErpSpace:
             return self.functions[name](*args, **kwargs)
         raise Exception(f"Function '{name}' is not registered in erpspace.")
 
+    @staticmethod
+    def share_doc(doc):
+        if doc.workflow_state not in ["Draft", "Rejected"]:
+            # Fetch custom role formula
+            formulas = frappe.db.sql(
+                """
+                SELECT t.custom_role_formula, t.custom_role
+                FROM `tabWorkflow Transition` t 
+                WHERE t.parent = %s AND t.state = %s AND t.action = 'Approve'
+                """,
+                (doc.doctype, doc.workflow_state),
+                as_dict=True
+            )
+
+            # Evaluate the formula with restricted globals
+            role = ""
+            if formulas[0].get("custom_role"):
+                role = formulas[0].get("custom_role")
+            else:
+                role = frappe.safe_eval(
+                    formulas[0].get("custom_role_formula"),
+                    get_workflow_safe_globals(), dict(doc=doc.as_dict())
+                )
+
+            # Fetch emails of users with the specified role
+            emails = frappe.db.sql(
+                """
+                SELECT u.name AS email
+                FROM `tabUser` u 
+                INNER JOIN `tabHas Role` r ON r.parent = u.name
+                WHERE r.role = %s
+                """,
+                (role,),
+                as_dict=True
+            )
+
+            # Share the document with users
+            for email_entry in emails:
+                email = email_entry.get("email")
+                frappe.share.add_docshare(
+                    doc.doctype, doc.name, email, submit=1, flags={"ignore_share_permission": True}
+                )
+
+                send_email(email, doc.doctype, doc.name)
+            
+
 # Create a global instance of ErpSpace
 erpspace = ErpSpace()
+
